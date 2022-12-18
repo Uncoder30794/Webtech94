8654f94fd5ae1b4830c82e7be4f8af17f377e566
vishal@podiotracker <uncoder30794@gmail.com>
2022-12-18 08:30:25 -0800

    Added Tailwind Css in it

--- web94app/src/app/app.module.ts
+++ web94app/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/auth/login/login.component';
+import { SignupComponent } from './componets/auth/signup/signup.component';
+import { ErrorComponent } from './componets/auth/error/error.component';
+import { HomeComponent } from './componets/pages/home/home.component';
+import { AboutComponent } from './componets/pages/about/about.component';
+import { ContactComponent } from './componets/pages/contact/contact.component';
+import { BlogComponent } from './componets/pages/blog/blog.component';
+import { ServiceComponent } from './componets/pages/service/service.component';
+import { NavbarComponent } from './componets/pages/navbar/navbar.component';
+import { FooterComponent } from './componets/pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ErrorComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    BlogComponent,
+    ServiceComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
